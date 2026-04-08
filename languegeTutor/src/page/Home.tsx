6129924ca4167ab43a3043eba4b1089cc79f4206
iOrlly <coelhoabnt2@gmail.com>
2026-04-08 19:40:17 -0300

    criado componentes button e sidebar, agregando com as funções de histórico, interação com o usuário, voice do navegador(SpeechSynthesisVoice). Adicionando css aos buttons, box e padrão do app(index.css).

--- languegeTutor/src/page/Home.tsx
+++ languegeTutor/src/page/Home.tsx
@@ -1,8 +1,17 @@
-import Box from '../components/Box.tsx'
+import Box from '../components/box/Box.tsx';
+import Button from '../components/button/Button.tsx';
+import Sidebar from '../components/historico/Sidebar.tsx';
+import { useState } from 'react'
 
 function Home() {
+    const [historicoAberto, setHistoricoAberto] = useState(false)
+    const handleToggleAberto = () => {
+        setHistoricoAberto((aberto) => !aberto);
+    }
     return(
-        <div id="container">
+        <div>
+            <Sidebar aberto={ historicoAberto }/>
+            <Button onToggleHistorico={ handleToggleAberto }/>
             <Box />
         </div>
     )
